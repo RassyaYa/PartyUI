8efb073c2f12326d53dcc84d8eab0ebe77e15979
VsrStudio <117354752+RassyaYa@users.noreply.github.com>
2024-10-24 19:01:22 +0700

    Create PartyManager.php

---
+++ src/PartyUI/PartyManager.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace PartyPlugin;
+
+use pocketmine\player\Player;
+use PartyPlugin\Party;
+
+class PartyManager {
+
+    private static $instance;
+    private $parties = [];
+
+    public function __construct() {
+        self::$instance = $this;
+    }
+
+    public static function getInstance(): PartyManager {
+        return self::$instance;
+    }
+
+    public function createParty(Player $player): void {
+        if ($this->isInParty($player)) {
+            $player->sendMessage("You are already in a party!");
+            return;
+        }
+        $this->parties[$player->getName()] = new Party($player);
+    }
+
+    public function invitePlayer(Player $leader, Player $invited): void {
+        $party = $this->getPlayerParty($leader);
+        if ($party !== null && $party->isLeader($leader)) {
+            $party->invite($invited);
+        } else {
+            $leader->sendMessage("You are not the leader of a party!");
+        }
+    }
+
+    public function kickPlayer(Player $leader, Player $target): void {
+        $party = $this->getPlayerParty($leader);
+        if ($party !== null && $party->isLeader($leader)) {
+            $party->kick($target);
+        } else {
+            $leader->sendMessage("You are not the leader of a party!");
+        }
+    }
+
+    public function leaveParty(Player $player): void {
+        $party = $this->getPlayerParty($player);
+        if ($party !== null) {
+            $party->removeMember($player);
+        } else {
+            $player->sendMessage("You are not in a party!");
+        }
+    }
+
+    public function getPlayerParty(Player $player): ?Party {
+        foreach ($this->parties as $party) {
+            if ($party->isMember($player)) {
+                return $party;
+            }
+        }
+        return null;
+    }
+
+    public function isInParty(Player $player): bool {
+        return $this->getPlayerParty($player) !== null;
+    }
+
+    public function showJoinPartyUI(Player $player): void {
+        // Implement UI for listing parties to join
+    }
+
+    public function listParties(Player $player): void {
+        // Display available parties to the player
+    }
+
+    public function showPartyMembers(Player $player): void {
+        $party = $this->getPlayerParty($player);
+        if ($party !== null) {
+            $members = $party->getMembers();
+            $player->sendMessage("Party members: " . implode(", ", $members));
+        } else {
+            $player->sendMessage("You are not in a party!");
+        }
+    }
+}
